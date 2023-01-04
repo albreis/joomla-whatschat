e9a4cebd560d44f74d3d5823df573c36e6c8ed76
Albreis - Design & Programação <contato@albreis.com.br>
2023-01-04 18:33:38 -0300

    Remover necessidade do font awesome

--- mod_whatschat.php
+++ mod_whatschat.php
@@ -16,7 +16,6 @@ $lang       = JFactory::getLanguage();
 $app        = JFactory::getApplication();
 $doc = JFactory::getDocument();
 $doc->addScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js');
-$doc->addScript('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/brands.min.css');
 $set_Itemid = (int) $params->get('set_itemid', 0);
 $mitemid    = $set_Itemid > 0 ? $set_Itemid : $app->input->getInt('Itemid');
 require JModuleHelper::getLayoutPath('mod_whatschat', $params->get('layout', 'default'));
